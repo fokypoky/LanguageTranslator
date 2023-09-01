3a11ca904a8ede4a654749bb47b2b73f8ac72df3
Anton Petukhov <petuhova805@gmail.com>
2023-09-01 13:36:06 +0300

    Модуль, считающий значения переменных

---
+++ LanguageLib/Computing/Implementation/LanguageComputer.cs
@@ -0,0 +1,23 @@
+﻿using LanguageLib.AST.Interfaces;
+using LanguageLib.Computing.Interfaces;
+using LanguageLib.Tokens.Interfaces;
+
+namespace LanguageLib.Computing.Implementation
+{
+    public class LanguageComputer : ILanguageComputer
+    {
+        public List<IVariableToken> ResultList { get; set; }
+        public IAST AST { get; set; }
+        
+        public LanguageComputer(IAST ast)
+        {
+            AST = ast;
+            ResultList = new List<IVariableToken>();
+        }
+
+        public void Compute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LanguageLib/Computing/Interfaces/ILanguageComputer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LanguageLib.AST.Interfaces;
+using LanguageLib.Tokens.Interfaces;
+
+namespace LanguageLib.Computing.Interfaces
+{
+    public interface ILanguageComputer
+    {
+        List<IVariableToken> ResultList { get; set; } 
+        IAST AST { get; set; }
+        void Compute();
+    }
+}
