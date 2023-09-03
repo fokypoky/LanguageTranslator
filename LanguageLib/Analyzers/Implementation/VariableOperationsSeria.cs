9fa8d0f3b37cdc6b503a6e0017619a72236f3d72
Anton Petukhov <petuhova805@gmail.com>
2023-09-03 17:26:06 +0300

    Variable operation seria

---
+++ LanguageLib/Analyzers/Implementation/VariableOperationsSeria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageLib.Analyzers.Implementation
+{
+    public struct VariableOperationsSeria
+    {
+        public int StartIndex { get; set; }
+        public int EndIndex { get; set; }
+    }
+}
---
+++ LanguageLib/Analyzers/Implementation/VariableOperationsSeriaTypeEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageLib.Analyzers.Implementation
+{
+    public enum VariableOperationsSeriaType
+    {
+        PlusMinus, MultiplyDivision, Function
+    }
+}
