9cdb8be100be88971bb7ac6de5eb0f860d9baf49
Anton Petukhov <petuhova805@gmail.com>
2023-09-05 16:28:57 +0300

    Input binding

--- LanguageUI.WPF/ViewModels/MainWindowViewModel.cs
+++ LanguageUI.WPF/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using LanguageLib.Errors.Interfaces;
+using LanguageLib.Tokens.Interfaces;
+using LanguageUI.WPF.Inftastructure.Commands;
 using LanguageUI.WPF.ViewModels.Base;
 
 namespace LanguageUI.WPF.ViewModels
 {
     public class MainWindowViewModel : ViewModel
     {
+        private ObservableCollection<IError> _errors;
+        private ObservableCollection<IVariableToken> _variables;
+
+        private string _inputText;
+
+        public string InputText
+        {
+            get => _inputText;
+            set => SetField(ref _inputText, value);
+        }
+
+        #region Public collections
+
+        public ObservableCollection<IError> Errors
+        {
+            get => _errors;
+            set => SetField(ref _errors, value);
+        }
+
+        public ObservableCollection<IVariableToken> Variables
+        {
+            get => _variables;
+            set => SetField(ref _variables, value);
+        }
+
+        #endregion
+
+        public ICommand HandleCodeCommand
+        {
+            get => new RelayCommand((object parameter) =>
+            {
+
+            });
+        }
+
+        public MainWindowViewModel()
+        {
+            Errors = new ObservableCollection<IError>();
+            Variables = new ObservableCollection<IVariableToken>();
+        }
 
     }
 }
