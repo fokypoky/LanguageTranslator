39d6b13fe74eb4038d9537e32ee3054f13cb726d
Anton Petukhov <petuhova805@gmail.com>
2023-09-05 15:44:02 +0300

    MVVM implementation

---
+++ LanguageUI.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LanguageUI.WPF.ViewModels.Base;
+
+namespace LanguageUI.WPF.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+
+    }
+}
